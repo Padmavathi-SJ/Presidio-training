2d96d853b29dfb9eecccc7f6b3fa7919e53a26e8
Padmavathi-SJ <padmasj54@gmail.com>
2026-06-26 17:25:25 +0530

    worker & workerFields components done

--- AgriAndFarmMonitoring/AgriculturePlatform/AgriculturePlatform.Infrastructure/obj/Debug/net8.0/AgriculturePlatform.Infrastructure.AssemblyInfo.cs
+++ AgriAndFarmMonitoring/AgriculturePlatform/AgriculturePlatform.Infrastructure/obj/Debug/net8.0/AgriculturePlatform.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgriculturePlatform.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5c749ff244aa9a22b2a430b20c370ca48eff0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c586252671490e1cad74e33de1aa42a176dcefb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgriculturePlatform.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgriculturePlatform.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
